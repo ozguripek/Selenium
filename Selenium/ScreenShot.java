4c2efbd4e3b1c81c34e4f1aea9ae1b3bd8b8e3f8
ozguripek <ozgripek@gmail.com>
2020-11-30 03:31:11 +0100

    Selenium

--- Selenium/ScreenShot.java
+++ Selenium/ScreenShot.java
@@ -1,15 +1,19 @@
 package Selenium;
-import java.io.File;
-import java.io.IOException;
-
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.os.WindowsUtils;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.apache.commons.io.FileUtils;
 
-public class Miscelleanous {
+import java.io.File;
+import java.io.IOException;
+
+
+public class ScreenShot {
 
 	public static void main(String[] args) throws IOException {
 
@@ -18,19 +22,17 @@ public class Miscelleanous {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 
-		WindowsUtils.killByName("excel.exe");
-
 		// driver.manage().deleteCookieNamed("sessionKey");
 
 		// click on any link
 		// login page- verify login url
 
-		/*
-		 * driver.get("http://google.com");
-		 * 
-		 * File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		 * FileUtils.copyFile(src,new File("C:\\Users\\rahul\\screenshot.png"));
-		 */
+		 driver.get("http://google.com");
+		 
+		 File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		 FileUtils.copyFile(src,new File("//Users//imac//Documents//screenshot.png"));
+		 
+		
 
 	}
 
---
+++ Selenium/SslCertifications.java
@@ -0,0 +1,29 @@
+package Selenium;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.apache.commons.io.FileUtils;
+public class SslCertifications {
+
+	public static void main(String[] args) {
+		//SSl certificates
+
+		//Desired capabilities=
+		//general chrome profile
+		DesiredCapabilities ch=DesiredCapabilities.chrome();
+		//ch.acceptInsecureCerts();
+		ch.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		ch.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+		//Belows to your local browser
+		ChromeOptions c= new ChromeOptions();
+		c.merge(ch);
+		System.setProperty("webdriver.chrome.driver", "//Users//imac//Documents//Chrome-Driver//chromedriver");
+		WebDriver driver=new ChromeDriver(c);
+
+	}
+
+}
